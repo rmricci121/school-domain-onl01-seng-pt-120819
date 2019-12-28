c308c0125ff93bbf195ace546a858c7c8a467189
Robert M Ricci <rmricci121@gmail.com>
2019-12-28 12:05:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,13 @@
 class School
   
-  attr_reader :name
-  attr_accessor :roster
+  attr_reader :name, :roster
+   
   
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-  def add_student=
+  
   
 end
